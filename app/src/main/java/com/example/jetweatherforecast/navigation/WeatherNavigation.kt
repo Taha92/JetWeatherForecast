7d645dfcb7b9e77d5b17dcb7abd6d36aad02ffc6
Taha Imtiaz <taha@inspakt.com>
2024-06-28 14:02:48 +0300

    Animation for splash screen

--- app/src/main/java/com/example/jetweatherforecast/navigation/WeatherNavigation.kt
+++ app/src/main/java/com/example/jetweatherforecast/navigation/WeatherNavigation.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.jetweatherforecast.screen.MainScreen
 import com.example.jetweatherforecast.screen.WeatherSplashScreen
 
 @Composable
@@ -13,6 +14,10 @@ fun WeatherNavigation() {
         startDestination = WeatherScreens.SplashScreen.name) {
         composable(WeatherScreens.SplashScreen.name) {
             WeatherSplashScreen(navController = navController)
+        }
+
+        composable(WeatherScreens.MainScreen.name) {
+            MainScreen(navController = navController)
         }
     }
 }
