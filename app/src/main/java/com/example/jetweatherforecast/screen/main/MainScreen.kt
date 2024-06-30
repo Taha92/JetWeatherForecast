d6a0671b9bdb58cda75d5bd72b0b832172935f86
Taha Imtiaz <taha@inspakt.com>
2024-06-30 20:00:02 +0300

    Top data with icons

--- app/src/main/java/com/example/jetweatherforecast/screen/main/MainScreen.kt
+++ app/src/main/java/com/example/jetweatherforecast/screen/main/MainScreen.kt
@@ -1,18 +1,42 @@
 package com.example.jetweatherforecast.screen.main
 
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
+import com.example.jetweatherforecast.R
 import com.example.jetweatherforecast.data.DataOrException
 import com.example.jetweatherforecast.model.Weather
+import com.example.jetweatherforecast.model.WeatherItem
+import com.example.jetweatherforecast.util.formatDate
+import com.example.jetweatherforecast.util.formatDecimals
 import com.example.jetweatherforecast.widget.WeatherAppBar
 
 @Composable
@@ -22,7 +46,7 @@ fun MainScreen(navController: NavController,
     val weatherData = produceState<DataOrException<Weather, Boolean, Exception>>(
         initialValue = DataOrException(loading = true)
     ) {
-        value = mainViewModel.getWeatherData(city = "Seattle")
+        value = mainViewModel.getWeatherData(city = "istanbul")
     }.value
 
     if (weatherData.loading == true) {
@@ -34,18 +58,121 @@ fun MainScreen(navController: NavController,
 
 @Composable
 fun MainScaffold(weather: Weather, navController: NavController) {
-    Scaffold(topBar = {
+    Scaffold(
+        topBar = {
         WeatherAppBar(title = weather.city.name + " ,${weather.city.country}",
             navController = navController,
             elevation = 5.dp) {
             Log.e("TAG", "MainScaffold: Button Clicked")
         }
     }) {
-        MainContent(data = weather)
+        Column(modifier = Modifier.padding(top = it.calculateTopPadding())) {
+            MainContent(data = weather)
+        }
     }
 }
 
 @Composable
 fun MainContent(data: Weather) {
-    Text(text = data.city.name)
+    val weatherItem = data.list[0]
+    val imageUrl = "https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png"
+
+    Column(modifier = Modifier
+        .padding(4.dp)
+        .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = formatDate( weatherItem.dt),
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSecondaryContainer,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(6.dp)
+        )
+
+        Surface(modifier = Modifier
+            .padding(4.dp)
+            .size(200.dp),
+            shape = CircleShape,
+            color = Color(0xFFFFC400)
+        ) {
+           Column(
+               verticalArrangement = Arrangement.Center,
+               horizontalAlignment = Alignment.CenterHorizontally
+           ) {
+               WeatherStateImage(imageUrl = imageUrl)
+               Text(
+                   text = formatDecimals(weatherItem.temp.day) + "º",
+                   style = MaterialTheme.typography.headlineMedium,
+                   fontWeight = FontWeight.ExtraBold
+               )
+               Text(
+                   text = weatherItem.weather[0].main,
+                   fontStyle = FontStyle.Italic
+               )
+           }
+        }
+        HumidityWindPressureRow(weather = weatherItem)
+        Divider()
+    }
+
+
+}
+
+@Composable
+fun HumidityWindPressureRow(weather: WeatherItem) {
+    Row(modifier = Modifier
+        .padding(12.dp)
+        .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(modifier = Modifier
+            .padding(4.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.humidity),
+                contentDescription = "humidity icon",
+                modifier = Modifier.size(20.dp)
+            )
+            Text(
+                text = "${weather.humidity}%",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+
+        Row() {
+            Icon(
+                painter = painterResource(id = R.drawable.pressure),
+                contentDescription = "pressure icon",
+                modifier = Modifier.size(20.dp)
+            )
+            Text(
+                text = "${weather.pressure} psi",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+
+        Row() {
+            Icon(
+                painter = painterResource(id = R.drawable.wind),
+                contentDescription = "wind icon",
+                modifier = Modifier.size(20.dp)
+            )
+            Text(
+                text = "${weather.humidity} mph", //TODO Check this
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+}
+
+@Composable
+fun WeatherStateImage(imageUrl: String) {
+    Image(
+        painter = rememberImagePainter(imageUrl),
+        contentDescription = "icon image",
+        modifier = Modifier
+            .size(80.dp)
+    )
 }
